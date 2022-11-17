ed5c07504aba3cba62237b75440dcabfa1e93a37
Dungtt211203 <dungttph25462@fpt.edu.vn>
2022-11-17 10:09:28 +0700

    UPDATE

---
+++ PRO1041_IT17320_NH3/src/pro1041_it17320_nh3/PRO1041_IT17320_NH3.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pro1041_it17320_nh3;
+
+/**
+ *
+ * @author Admin
+ */
+public class PRO1041_IT17320_NH3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("uuuu");
+    }
+    
+}
